0f50aaa13fc9ed0fa4ee083670a81fadbb7a6d13
Ajiniss <baptistekenobi@gmail.com>
2024-12-01 18:35:26 +0100

    test

--- day1/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-    cout << "Hello AOC 2024!" << endl;
-    return 0;
-}
---
+++ day1/src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream> 
+using namespace std;
+
+int main()
+{
+ifstream file("list.txt");
+string line;
+
+if(file.is_open()){
+  while(getline(file, line)){
+    
+  }
+
+}
+
+
+return 0;
+}
